fdf74caa273e06035c9de6505adf5dd8b9094aef
Yuichi Murata <mrk21info+github@gmail.com>
2014-08-02 21:59:09 +0900

    Became to import the `__future__` module.

--- test/test_double/ansible_module_mock.py
+++ test/test_double/ansible_module_mock.py
@@ -1,4 +1,9 @@
 # -*- coding: utf-8 -*-
+from __future__ import print_function
+from __future__ import unicode_literals
+from __future__ import absolute_import
+from __future__ import generators
+from __future__ import division
 
 class AnsibleModuleMock(object):
     def __init__(self, argument_spec, bypass_checks=False, no_log=False,
--- test/test_double/http_client_mock.py
+++ test/test_double/http_client_mock.py
@@ -1,6 +1,13 @@
 # -*- coding: utf-8 -*-
+from __future__ import print_function
+from __future__ import unicode_literals
+from __future__ import absolute_import
+from __future__ import generators
+from __future__ import division
+
 import httplib
 import socket
+
 from test_double.ansible_module_mock import *
 
 class HTTPClientMock(object):
